8c6ac624eb3214bfa1296943f074ed1fd7ecb3dd
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-07-12 10:14:47 +0530

    using data files

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import contacts from '../data/contact.json';
 import ExternalLinks from './ExternalLinks';
 export default class Contact extends React.Component{
-    render(){
-        return(
-            <div>
-                <ExternalLinks link="tel:+917013765669" name="mobile" />
-                <ExternalLinks link="https://www.linkedin.com/in/sharshach/" name="linkedin" />
-                <ExternalLinks link="mailto:sharshach@gmail.com" name="mail" />
-                <ExternalLinks link="https://wa.me/+917013765669" name="whatsapp" />
-            </div>
+    render() {
+        console.log(contacts);
+        return (
+            contacts ?contacts.map(contact => 
+                <ExternalLinks name={contact.name} link={contact.link} />
+            ):<div/>
         );
     }
 }
