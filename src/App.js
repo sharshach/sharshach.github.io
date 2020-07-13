f9b63257eaa22a17ba0c4621fbfe719795078c6b
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-07-13 14:44:54 +0530

    added projects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./App.css";
-import Contact from "./components/Contact";
-import Project from "./components/Project";
-import Jobs from "./components/Jobs";
+import Projects from "./components/Projects/Projects";
+import Jobs from "./components/Jobs/Jobs";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = (_props) => (
   <div className="App">
-    <Jobs />
-    <Project />
-    <Contact />
+    <Header />
+        <div style={{"height":"auto"}}>
+        <Jobs />
+        <Projects />
+        </div>
+    <Footer />
   </div>
 );
 
