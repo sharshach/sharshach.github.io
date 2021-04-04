70f13264f5cc52c4af131c69f170b5528775fe1c
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2021-04-04 12:58:35 +0530

    github stats added

---
+++ src/components/Github/GithubStats.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import data from '../../data/github-stats.json'
+import './github-stats.css';
+import '../style.css';
+
+export default function GithubStats() {
+    return (
+        <div className="github-stats">
+            <div className="github-stats-left">
+            <div className="my-heading">GitHub</div>
+                <div className="margin-10">
+                    <img alt="Stats" src="https://github-readme-stats.vercel.app/api?username=sharshach&theme=highcontrast&show_icons=true&count_private=true"/>          
+                </div>
+                <div className="margin-10">
+                    <img alt="languages" src="https://github-readme-stats.vercel.app/api/top-langs/?username=sharshach&theme=highcontrast&hide=html" />
+                </div>
+            </div>
+            <div className="github-stats-right">
+            {
+                data.map(item=>(
+                    <div className="margin-10">
+                    <a href={`https://github.com/${item.user}/${item.repo}`}>
+                        <img alt={item.repo} src={`https://github-readme-stats.vercel.app/api/pin/?username=${item.user}&repo=${item.repo}&theme=highcontrast`}/>
+                    </a>
+                    </div>
+                ))
+            }
+            <a href="https://github.com/sharshach" target="blank">
+            <button style={{width:"200px"}}>More Projects </button>
+            </a>
+            </div>
+        </div>
+    )
+}
+
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,34 @@ import "./Header.css"
 export default class Header extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            x:0,
+            y:0,
+        };
     }
-
+    componentDidMount(){
+        setInterval(this.moveStar, 1);
+    }
+    moveStar=()=>{
+        const {x,y}=this.state;
+        this.setState({x:(x+Math.random()-0.5)%100,y:(y+Math.random()-0.5)%100});
+    }
+    // getAllDots=()=>{
+    //     let i=10,j=10;
+        
+    // }
     render() {
         return (
             <div className="header-main">
-                <h1>CHILUKURI SRI HARSHA</h1>
+                {/* <div className="header-tiles">
+                <div className="header-tile">Projects</div>
+                <div className="header-tile">Projects</div>
+                <div className="header-tile">Projects</div>
+                <div className="header-tile">Projects</div>
+                </div> */}
+              
+                <div className="header-dot" style={{left:`${this.state.x}vw`,top:`${this.state.y}vh`}}/>
+                <div className="header-dot" style={{left:`${this.state.x}vw`,top:`${this.state.y}vh`}}/>
             </div>
         )
     }
